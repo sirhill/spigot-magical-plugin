0b7199b88a79ecc53cf7debedb941d1d4d410624
Cyril Lapinte <cyril.lapinte@free.fr>
2016-01-23 14:48:21 +0100

    Initializing repository

---
+++ src/main/java/magical/MagicalPlugin.java
@@ -0,0 +1,30 @@
+package magical;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MagicalPlugin extends JavaPlugin {
+    // This code is called after the server starts and after the /reload command
+    @Override
+    public void onEnable() {
+        getLogger().log(Level.INFO, "{0}.onEnable()", this.getClass().getName());
+        getServer().getPluginManager().registerEvents(new MagicalPluginListener(), this);
+    }
+
+    // This code is called before the server stops and after the /reload command
+    @Override
+    public void onDisable() {
+        getLogger().log(Level.INFO, "{0}.onDisable()", this.getClass().getName());
+    }
+}
---
+++ src/main/java/magical/MagicalWand.java
@@ -0,0 +1,33 @@
+package magical;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.Utility;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.material.MaterialData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MagicalWand extends ItemStack {
+
+    public final static String DISPLAY_NAME = ChatColor.RED + "" + ChatColor.BOLD + "Magical Wand";
+
+    public MagicalWand() {
+        super(Material.STICK);
+        ItemMeta itemMeta = getItemMeta();
+        itemMeta.setDisplayName(DISPLAY_NAME);
+        itemMeta.addEnchant(Enchantment.FIRE_ASPECT, 0, false);
+        List<String> lore = new ArrayList<String>();
+        lore.add(ChatColor.DARK_AQUA + "Lost by Harry");
+        itemMeta.setLore(lore);
+        setItemMeta(itemMeta);
+    }
+
+    @Utility
+    public int getMaxStackSize() {
+        return 1;
+    }
+}
